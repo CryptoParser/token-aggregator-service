b4dfe592bbea71987e4071500d863f7520b4e90e
artokit <kdsa3102@gmail.com>
2025-11-12 00:54:52 +0300

    chages

---
+++ TokenAggregator.Api/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenAggregator.Api.Controllers;
+
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet("ping")]
+    public async Task<IActionResult> Test()
+    {
+        return Ok("pong");
+    }
+}
